e04f97412ded27a43de6ec9a1b69b7bebba14cf2
luis.villar <luis.villar@next-technologies.com.mx>
2023-05-17 16:45:00 -0600

    2 more

--- utils.js
+++ utils.js
@@ -1,7 +1,9 @@
 const arr = [
-   { id: 1, status: true, user: 'user-1' }
+   { id: 1, status: true, user: 'user-1' },
+   { id: 2, status: true, user: 'user-2' }
 ];
 
 const arr2 = [
    { id: 1, status: true, user: 'USER-1' }
+   { id: 2, status: true, user: 'USER-2' }
 ];
