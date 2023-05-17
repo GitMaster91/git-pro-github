95a3bccdfc2370eeca6ddaac18f1168e390a9670
luis.villar <luis.villar@next-technologies.com.mx>
2023-05-17 16:50:13 -0600

    stashed files and created a new branch

---
+++ helpers.js
@@ -0,0 +1,5 @@
+const helper1 = (obj) => { 
+   return obj.user.toUpperCase();
+};
+
+console.log(helper1({user: 'luis'}));
---
+++ valids.js
@@ -0,0 +1,5 @@
+const valids = [
+   { id: 1, valid: 100 },
+   { id: 2, valid: 200 },
+   { id: 3, valid: 300 }
+];
