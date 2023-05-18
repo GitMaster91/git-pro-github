663ac956a14a256a3288551d230b3e3d9ee27327
luis.villar <luis.villar@next-technologies.com.mx>
2023-05-18 12:25:33 -0600

    relevantFn

--- utils.js
+++ utils.js
@@ -7,3 +7,12 @@ const arr2 = [
    { id: 1, status: true, user: 'USER-1' },
    { id: 2, status: true, user: 'USER-2' }
 ];
+
+function relevantFn(obj) {
+   return `id: ${obj.id} name: ${obj.user.toUpperCase()}`;
+};
+
+console.log(relevantFn({
+   id  : 1,
+   user: 'luis'
+}));
